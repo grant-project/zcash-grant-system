a8ca473d7d635f4d90a43b08ad8b9bb8e87ecbd1
Danny Skubak <skubakdj@gmail.com>
2019-10-21 16:17:04 -0400

    disable milestones for proposals accepted without funding

--- frontend/client/components/Proposal/index.tsx
+++ frontend/client/components/Proposal/index.tsx
@@ -101,6 +101,10 @@ export class ProposalDetail extends React.Component<Props, State> {
 
     const isTrustee = !!proposal.team.find(tm => tm.userid === (user && user.userid));
     const isLive = proposal.status === STATUS.LIVE;
+    const milestonesDisabled = proposal.isVersionTwo
+      ? !proposal.acceptedWithFunding
+      : false;
+    const defaultTab = milestonesDisabled ? 'discussions' : 'milestones';
 
     const adminMenu = (
       <Menu>
@@ -230,7 +234,7 @@ export class ProposalDetail extends React.Component<Props, State> {
         </div>
 
         <div className="Proposal-bottom">
-          <LinkableTabs scrollToTabs defaultActiveKey="milestones">
+          <LinkableTabs scrollToTabs defaultActiveKey={defaultTab}>
             <Tabs.TabPane tab="Milestones" key="milestones">
               <div style={{ marginTop: '1.5rem', padding: '0 2rem' }}>
                 <Milestones proposal={proposal} />
