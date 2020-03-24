1adabf4ab01165055d07c71dfa6de2b68b5b9f89
Daniel Ternyak <dternyak@gmail.com>
2020-03-24 15:00:26 -0500

    #5

--- backend/grant/patches.py
+++ backend/grant/patches.py
@@ -1,19 +1,9 @@
 import inspect
 import textwrap
 
-from werkzeug.wrappers.base_response import BaseResponse
+from werkzeug.wrappers import base_response
+from .base_response_fork import BaseResponse
 
 
 def patch_werkzeug_set_samesite():
-    BaseResponse_method_source = inspect.getsource(BaseResponse.set_cookie)
-
-    BaseResponse_method_source = textwrap.dedent(BaseResponse_method_source)
-
-    # Patch the code
-    BaseResponse_method_source = BaseResponse_method_source.replace('samesite=None', 'samesite="None"')
-
-    # This creates the function `patched_fn`
-    patched_fn = exec(BaseResponse_method_source)
-
-    # Replace the original function
-    BaseResponse.set_cookie = patched_fn
+    base_response.BaseResponse = BaseResponse
